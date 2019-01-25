83232fc0dd186b0867a4443510fc19e4dd1d3fb1
luglio22 <sweet.c.july22@gmail.com>
2019-01-25 16:48:39 +0900

    自動更新の実装
    WHAT
    チャット画面の自動更新の実装
    
    WHY
    メッセージのやりとりをスムーズに行う為

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
 
   function buildHTML(message){
     var image = message.image ? `<img src="${message.image}" class="chat-main__message-body"> ` : "" ;
-    var html = `<div class="chat-main__message">
+    var html = `<div class="chat-main__message"data-id=${message.id}>
                 <div class="chat-main__message-name">
                 ${message.name}
                 </div>
@@ -44,4 +44,33 @@ $(function(){
       alert('メッセージの送信に失敗しました');
     })
   })
-})
+
+
+  //自動更新
+  $(function(){
+    if(location.href.match(/\/groups\/\d+\/messages/)){
+      setInterval(update,5000);
+    }
+  });
+  function update(){
+      var message_id = $('.chat-main__message').last().data('id');
+    $.ajax({
+      url: location.href,
+      type: 'GET',
+      data: {id: message_id},
+      dataType: 'json'
+    })
+    .done(function(message){
+      message.forEach(function(message){
+        var html = buildHTML(message);
+        $('.chat-main__body--message-list').append(html);
+        $('.chat-main__body').animate({scrollTop: $('.chat-main__body')[0].scrollHeight
+      })
+      })
+    })
+
+    .fail(function(){
+      alert('自動更新に失敗しました')
+    })
+  };
+});
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,4 +1,5 @@
 json.name            @message.user.name
-json.strftime      @message.created_at
+json.strftime        @message.created_at.strftime('%Y/%m/%d %H:%M')
 json.content         @message.content
 json.image           @message.image.url
+json.id              @message.id
