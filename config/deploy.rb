03481ce0b67db5571496e8b08772927572ee98dd
luglio22 <sweet.c.july22@gmail.com>
2019-01-27 19:06:57 +0900

    変更
    変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '<Capistranoのバージョン>'
+lock '3.11.0'
 
 set :application, 'chat-space'
 set :repo_url,  'git@github.com:luglio22/chat-space.git'
